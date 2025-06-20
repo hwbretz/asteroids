6630959182f0871e7be088302b07f9d64b8a726e
hwbretz <hwbretz@gmail.com>
2025-06-20 06:03:56 -0500

    triangle 'player' added

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 #import pygame, so we can use thier library
 import pygame
 from constants import *
+from player import *
 
 def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
-    dt = 0   
+    dt = 0
+    # make a playerr object
+    player_sprite = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player_sprite.draw(screen)  
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        player_sprite.draw(screen)
         pygame.display.flip()
 
         dt = game_clock.tick(60) / 1000
